7ec1879bd7d7f7d6f15471557b8a3f60daec52e2
Kaan Kalkan <kalkank@itu.edu.tr>
2017-04-05 11:57:19 +0300

    added tirs layer stack support

--- l8layerStack.m
+++ l8layerStack.m
@@ -1,15 +1,24 @@
-dirList = subdir('E:\Landsat8\*.xml');
+dirList = subdir('D:\LC81810332016211LGN00-kurak-menemen\*.txt');
 for dirId = 1:size(dirList)
     dirList(dirId).name
     [pathstr,name,ext] = fileparts(dirList(dirId).name);
-    cd( pathstr )
-    for i = 1:7
-        [Img(:,:,i), R(i)] = geotiffread( [name '_sr_band' num2str(i) '.tif'] );
-        tiffinfo(i) = geotiffinfo([name '_sr_band' num2str(1) '.tif']);
+    if strcmp(name(end-2:end), 'MTL')
+        cd( pathstr )
+        name = name(1:end-4);
+        for i = 1:7
+            [Img(:,:,i), R(i)] = geotiffread( [name '_B' num2str(i) '.tif'] );
+            tiffinfo(i) = geotiffinfo([name '_B' num2str(1) '.tif']);
+        end
+        for i = 10:11
+            [ImgT(:,:,i), RT(i)] = geotiffread( [name '_B' num2str(i) '.tif'] );
+            tiffinfoT(i) = geotiffinfo([name '_B' num2str(1) '.tif']);
+        end
+        
+        copyfile([name '_MTL.txt'],[ '..\' name '_MTL.txt'])
+        geotiffwrite(['..\' name '_OLI' '.tif'], Img, R(1), 'GeoKeyDirectoryTag', tiffinfo(1).GeoTIFFTags.GeoKeyDirectoryTag);
+        geotiffwrite(['..\' name '_TIRS' '.tif'], ImgT, RT(1), 'GeoKeyDirectoryTag', tiffinfoT(1).GeoTIFFTags.GeoKeyDirectoryTag);
+        
+        clear Img; clear ImgT;
+        clear tiffinfo; clear tiffinfoT; clear R; clear RT;
     end
-    
-    
-    copyfile([name '_cfmask.tif'],[ '..\' name '_cfmask.tif'])
-    geotiffwrite(['..\' name '.tif'], Img, R(1), 'GeoKeyDirectoryTag', tiffinfo(1).GeoTIFFTags.GeoKeyDirectoryTag);
-    clear Img;
 end
